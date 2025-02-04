2d9af3e629de916c067e684ec3949c49c7fbce75
M0755 <24bit036@sot.pdpu.ac.in>
2025-02-04 15:32:12 +0530

    Add files via upload

---
+++ EXE3Program4.py
@@ -0,0 +1,5 @@
+#Q3
+str2=input("Enter a string")
+str3=input("Enter a string")
+str4=str2.replace(str3,'')
+print(str4)
---
+++ Loop Program.py
@@ -0,0 +1,23 @@
+for i in range(1,11):
+    print(i)
+
+#Q1
+str=input()
+vowels="aeiouAEIOU"
+vowel=0
+for ch in str:
+   if ch in vowels:
+       vowel+=1
+       print(vowel)
+
+
+#Q2
+str1=input("enter a name")
+str1=str1.upper()
+str1=str1.lower()
+str1=str1.swapcase()
+print(str1)
+
+
+        
+        
