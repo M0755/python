1c00f38d5c2585e4915e78b7c3fbdc3c806bc320
M0755 <24bit036@sot.pdpu.ac.in>
2025-02-17 22:24:13 +0530

    Create EXE4 P-8.py

---
+++ EXE4 P-8.py
@@ -0,0 +1,11 @@
+import math
+
+# Function to compute factorial
+def factorial(n):
+    return math.factorial(n)
+
+# Get input from the user
+num = int(input("Enter a number: "))
+
+# Compute and print factorial
+print(f"Factorial of {num} is {factorial(num)}")
