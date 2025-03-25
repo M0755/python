86ba55eebf3707b2f22406dabcdfaad43d246327
M0755 <24bit036@sot.pdpu.ac.in>
2025-03-25 15:54:59 +0530

    Add files via upload

---
+++ EXE9  P-A.py
@@ -0,0 +1,17 @@
+def fun():
+     print("This is the first function")
+
+def disp():
+    print("This is the second function")
+
+def msg():
+    print("This is the third function")
+
+l1=[fun(),disp(),msg()]
+
+for i in l1:
+     i
+     print("WTF")
+  
+    
+
