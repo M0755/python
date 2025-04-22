dba435936fc4cc4995e6760024c76698911305a0
M0755 <24bit036@sot.pdpu.ac.in>
2025-04-22 15:40:44 +0530

    Add files via upload

---
+++ 12.2.py
@@ -0,0 +1,46 @@
+class matrix:
+    def __init__(self):
+        self.m = [[0,0,0],[0,0,0],[0,0,0]]
+    def getMatrix(self):
+        print("Enter 3x3 Matrix:")
+        for i in range(3):
+            for j in range(3):
+                self.m[i][j] = int(input("enter the values:"))
+    def __str__(s):
+        for i in range(3):
+            for j in range(3):
+                print(s.m[i][j],end = ' ')
+            print('\n')
+        return " "
+a = matrix()
+print(a)
+a.getMatrix()
+print(a)
+
+def getMatrix1(self):
+        print("Enter 3x3 Matrix:")
+        for i in range(3):
+            for j in range(3):
+                self.m[i][j] = int(input("enter the values:"))
+                
+def __str__(s):
+        for i in range(3):
+            for j in range(3):
+                print(s.m[i][j],end = ' ')
+            print('\n')
+        return " "
+a.getMatrix()
+print(a)
+a=getMatrix()
+b=getMatrix1()
+def additionofMatrix(self):
+      return i[0]+i[0],i[1]+i[1]
+
+print(getMatrix.additionofMatrix(a,b))
+
+
+
+
+    
+                  
+        
